d9f5ddd38153ff5219e8dd934f994cbf4553b3a0
Abhik <theabhik2020@gmail.com>
2025-05-16 12:54:36 +0530

    main: Added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,13 @@
-import { log } from "@repo/logger";
-import { createServer } from "./server";
+import express, { Request, Response } from 'express';
 
-const port = process.env.PORT || 5001;
-const app = createServer();
+const app: express.Express = express();
+const PORT = process.env.PORT || 3000;
 
-// server.listen(port, () => {
-//   log(`api running on ${port}`);
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({ message: 'Hello from plain Express app!' });
+});
+//test
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
 // });
-
 export default app;
